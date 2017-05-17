81d4b97a713b8f82f60f6d63c82eb7c2762aade5
Indrek Luuk <indrek.luuk@gmail.com>
2017-05-17 21:15:50 +0300

    separated color calculation functions

--- src/PBlockIcons/src/palette/PaletteGenerator.h
+++ src/PBlockIcons/src/palette/PaletteGenerator.h
@@ -44,7 +44,6 @@ class PaletteGenerator : public PaletteGeneratorBase {
     uint16_t hueSection4 = hueSection * 4;
     uint16_t hueSection5 = hueSection * 5;
     float V;
-    float dS;
     RgbColor line[w];
 
 public:
@@ -52,8 +51,7 @@ public:
     PaletteGenerator(uint8_t scaleW, uint8_t scaleH, float V) :
         scaleW(scaleW),
         scaleH(scaleH),
-        V(V),
-        dS((1.0f / (w - 1.0f)) * V)
+        V(V)
     {
       reset(0, 0);
     }
@@ -68,7 +66,7 @@ public:
       curSaturation = x / scaleW;
       yCnt = y % scaleH;
       curHue = y / scaleH;
-      initLine(curHue);
+      initLineHS(curHue, V);
     }
 
     void nextLine() {
@@ -77,7 +75,7 @@ public:
       if (yCnt == scaleH) {
         yCnt = 0;
         curHue++;
-        initLine(curHue);
+        initLineHS(curHue, V);
       }
       yCnt++;
     };
@@ -92,63 +90,77 @@ public:
     };
 
 
-protected:
+private:
 
     RgbColor getPixel(uint8_t saturation) {
       return line[saturation];
     };
 
-    void initLine(uint8_t hue) {
+
+
+    void initLineHS(uint8_t hue, float value) {
+      float hueMultiplier = calculateHueMultiplier(hue);
+      float saturation = 0;
+      float dS = (1.0f / (w - 1.0f)) * value;
+
+      for (uint8_t saturationCnt = 0; saturationCnt < w; saturationCnt++) {
+        line[saturationCnt] = calculateRGB(hue, hueMultiplier, saturation, value);
+        saturation+=dS;
+      }
+    }
+
+
+
+    float calculateHueMultiplier(uint8_t hue) {
       float hueDiv = (hue / hueSection);
       uint16_t hueIntDiv = (uint16_t)hueDiv;
       float hueMod = hueIntDiv % 2 + (hueDiv - (hueIntDiv + (uint16_t)1));
-      float hueMultiplier = (1.0f - abs(hueMod));
+      return (1.0f - abs(hueMod));
+    }
 
-      float saturation = 0;
-      for (uint8_t saturationCnt = 0; saturationCnt < w; saturationCnt++) {
 
-        float X = saturation * hueMultiplier;
 
-        float Rp;
-        float Gp;
-        float Bp;
 
-        if (hue < hueSection1) {
-          Rp = saturation;
-          Gp = X;
-          Bp = 0;
-        } else if (hue < hueSection2) {
-          Rp = X;
-          Gp = saturation;
-          Bp = 0;
-        } else if (hue < hueSection3) {
-          Rp = 0;
-          Gp = saturation;
-          Bp = X;
-        } else if (hue < hueSection4) {
-          Rp = 0;
-          Gp = X;
-          Bp = saturation;
-        } else if (hue < hueSection5) {
-          Rp = X;
-          Gp = 0;
-          Bp = saturation;
-        } else {
-          Rp = saturation;
-          Gp = 0;
-          Bp = X;
-        }
+    RgbColor calculateRGB(uint8_t hue, float hueMultiplier, float saturation, float value) {
+      float X = saturation * hueMultiplier;
 
-        float m = V - saturation;
-        if (m < 0) m = 0;
+      float Rp;
+      float Gp;
+      float Bp;
 
-        line[saturationCnt] = RgbColor(
-            (Rp + m) * (uint16_t)31,
-            (Gp + m) * (uint16_t)63,
-            (Bp + m) * (uint16_t)31);
-
-        saturation+=dS;
+      if (hue < hueSection1) {
+        Rp = saturation;
+        Gp = X;
+        Bp = 0;
+      } else if (hue < hueSection2) {
+        Rp = X;
+        Gp = saturation;
+        Bp = 0;
+      } else if (hue < hueSection3) {
+        Rp = 0;
+        Gp = saturation;
+        Bp = X;
+      } else if (hue < hueSection4) {
+        Rp = 0;
+        Gp = X;
+        Bp = saturation;
+      } else if (hue < hueSection5) {
+        Rp = X;
+        Gp = 0;
+        Bp = saturation;
+      } else {
+        Rp = saturation;
+        Gp = 0;
+        Bp = X;
       }
+
+      float m = V - saturation;
+      if (m < 0) m = 0;
+
+      return RgbColor(
+          (Rp + m) * (uint16_t)31,
+          (Gp + m) * (uint16_t)63,
+          (Bp + m) * (uint16_t)31);
     }
 
 };
